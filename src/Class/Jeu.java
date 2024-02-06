987de0454e22ae0a309360b1427ca5f48b9a3058
Younès <Younes@mbp-de-youns.home>
2024-02-07 00:37:31 +0100

    Ajustements dans les class Jeu et Interface

---
+++ src/Class/Jeu.java
@@ -0,0 +1,44 @@
+package Class;
+
+public class Jeu {
+
+    public int[][] plateau = new int[12][13]; //
+
+    public Jeu() { // Constructeur
+        this.plateau = creerPlateau();
+    }
+
+    public int[][] creerPlateau() { // Fonction pour créer le tableau
+        int[][] plateau = new int[12][13];
+        int lignes = plateau.length;
+        int colonnes = plateau[0].length;
+
+        for(int i = 0; i < lignes; i++) { // Boucle pour parcourir les lignes du tableau
+            for(int j = 0; j < colonnes; j++) { // Boucle pour parcourir les colonnes du tableau
+                if(i == 0 || i == lignes - 1 || j == 0 || j == colonnes - 1) { // Condition pour vérifier si on est sur un bord
+                    plateau[i][j] = 1;
+                } else {
+                    plateau[i][j] = 0;
+                }
+            }
+        }
+        return plateau;
+    }
+
+    public void afficher(){ // Affiche le tableau
+        for(int i = 0; i < plateau.length; i++) {
+            for(int j = 0; j < plateau[i].length; j++) {
+                System.out.print(plateau[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Fonction principale pour tester la fonction creerTableau
+    public static void main(String[] args) {
+        Jeu plateau = new Jeu();
+
+        // Afficher le tableau
+        plateau.afficher();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+package Class;
+import java.lang.invoke.TypeDescriptor;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static Class.Interface.menu;
+import static Class.Jeu.*;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         int i = 0;
         menu(i);
+
+        Jeu plateau = new Jeu();
+
+        plateau.afficherPlateau();
+
     }
 }
