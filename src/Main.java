30db2efea266fd12fd4240e537092c1c58ad0d14
Nicolas <nico.dobosz@gmail.com>
2024-02-06 16:02:40 +0100

    add verif menu

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
 
-    public static void menu(){
+    public static int verificationEntier(){
+        /** Cette fonction permet à l'utilisateur d'entrer un entier
+         *  Elle permet de gérer les cas ou la valeur entrer ne correspond
+         *  pas à un entier
+         **/
+        int valeur = 0;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            valeur = scanner.nextInt();
+            return valeur;
+        }
+        catch (InputMismatchException e){ // Si la valeur n'est pas un entier
+            System.out.println("Veuillez entrer un chiffre ( Qu'est ce que tu dis de ça Clément )");
+        }
+        catch (Exception e){ // Si il y a une autre erreur quelconque
+            System.out.println("Veuillez entrer un chiffre ( Pas à moi CLément )");
+        }
+        return valeur;
+    }
+
+    public static void menu(int i){
         System.out.println(" \nBienvenue dans le menu");
         System.out.println("\n1 - Lire les règles");
         System.out.println("2 - Jouer au jeu");
         System.out.println("3 - Quitter");
-        Scanner choixOption = new Scanner(System.in);
         System.out.println("\nChoisissez une option : ");
-        int choix = choixOption.nextInt();
+        int choix = verificationEntier();
         if (choix == 1) {
+            if (i == 10) {
+                System.out.println("ça fait 10 fois que tu lis les règles c'est bon là non ?");
+            }
             System.out.println("Voici les règles");
-            menu();
+            menu(++i);
         }
         if (choix == 2) {
             System.out.println("Lancement jeu");
@@ -22,6 +45,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        menu();
+        int i = 0;
+        menu(i);
     }
 }
