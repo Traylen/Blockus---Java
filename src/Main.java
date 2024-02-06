ea282df735067114b0794c82c0a9229a64fdc431
Nicolas <nico.dobosz@gmail.com>
2024-02-06 15:20:45 +0100

    Menu V1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void menu(){
+        System.out.println(" \nBienvenue dans le menu");
+        System.out.println("\n1 - Lire les règles");
+        System.out.println("2 - Jouer au jeu");
+        System.out.println("3 - Quitter");
+        Scanner choixOption = new Scanner(System.in);
+        System.out.println("\nChoisissez une option : ");
+        int choix = choixOption.nextInt();
+        if (choix == 1) {
+            System.out.println("Voici les règles");
+            menu();
         }
+        if (choix == 2) {
+            System.out.println("Lancement jeu");
+        }
+        if (choix == 3) {
+            System.out.println("\n Deconnexion en cours");
+            System.exit(0);
+        }
+    }
+    public static void main(String[] args) {
+        menu();
     }
 }
