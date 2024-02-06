ed4ebbd037df0eb2d99c699e138d20573c111912
aurel95 <aurelien.traini@gmail.com>
2024-02-06 16:20:18 +0100

    plateau mieux + commentaire

--- src/plateau.java
+++ src/plateau.java
@@ -1,18 +1,28 @@
-class plateau {
-    /**
-     cette fonction permet de crée un tableau avec 11 colones et 10 ligne de 0
-     */
-    public static void main(String[] arg) {
-        int[][] tableau;
-        int i, j;
-        tableau = new int[10][11];
-        for (i = 0; i < tableau.length; i++) {
-            for (j = 0; j < tableau[i].length; j++) {
+ class Plateau {
+     /**
+      * création d'un tableau avec des bord en 1 avec une fonction pour l'afficher
+      */
+    public static void main(String[] args) {
+        int[][] tableau = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        };
 
+        for (int i = 0; i < tableau.length; i++) {
+            for (int j = 0; j < tableau[i].length; j++) {
                 System.out.print(tableau[i][j] + " ");
             }
-            System.out.println();
+            System.out.println(); // Aller à la ligne après chaque sous-tableau
         }
-        System.out.println();
     }
 }
